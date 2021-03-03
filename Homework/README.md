23537bc00e666f814af39ace5a85f24a4850ad62
AngelaPatiag <url.https://patiagangela@github.com>
2021-03-03 09:33:57 +0800

    First commit

---
+++ Homework/README.md
@@ -0,0 +1,12 @@
+# Homework #JSVIII
+
+## Instructions
+
+---
+
+1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old. Doing this will help you quickly discover any holes in your understanding. Ask your questions on Slack.
+
+   - Recursion
+     Recursion is an iteration that will continuosly call itself until the program crashes that is why it needs a condition that will make it stop when the condition returns false.
+
+2. From the top level of your `Precourse` folder, run `npm test L11/Homework/tests/JSVIII.test.js` to run the automated tests. You will fill out the functions in `homework.js` to make the tests pass.
---
+++ Homework/homework.js
@@ -0,0 +1,39 @@
+// Do not change any of the function names
+
+// solve these recursively
+
+function nFactorial(n) {
+  // return the factorial for n
+  // example:
+  // the factorial of 3 is 6 (3 * 2 * 1)
+  if (n < 0){
+    return -1;
+  }
+  else if (n === 0) {
+    return 1;
+  }
+  else {
+    return (n * nFactorial(n-1));
+  }
+nFactorial
+}
+
+function nFibonacci(n) {
+  // fibonacci sequence: 1 2 3 5 8 13 ...
+  // return the nth number in the sequence
+  if (n <= 1) {
+    return 1;
+  }
+  
+  return (nFibonacci(n-1)+nFibonacci(n-2));
+  }
+
+
+
+// Do not modify code below this line.
+// --------------------------------
+
+module.exports = {
+  nFactorial,
+  nFibonacci,
+};
